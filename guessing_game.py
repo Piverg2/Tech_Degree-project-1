31586be550f98fff46938ca57eab826b119c45b6
Piverg2 <gpiver@gmail.com>
2020-09-14 10:17:31 -0400

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,37 +1,35 @@
-"""
-Python Web Development Techdegree
-Project 1 - Number Guessing Game
---------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-"""
-
+# Python Web Development Techdegree
+# Project 1 - Number Guessing Game - Gavyn Piver
+# --------------------------------
 import random
 
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
-
-
-
-# Kick off the program by calling the start_game function.
+    print("  -------------------------------  ")
+    print("Welcome to the Number Guessing Game!")
+    print("  -------------------------------  ")
+    guess = int(input("Pick a number between 1 and 10: "))
+    tries = 1
+    # Had to look up how to use Random and randomint. Source: https://www.w3schools.com/python/ref_random_randint.asp
+    answer = random.randint(1, 10)
+    if guess == answer:
+      print("Awesome! you guessed the answer on the first!")
+    while guess != answer:
+      tries += 1
+      if guess > answer:
+        print("It's lower")
+      if guess < answer:
+        print("It's higher")
+      guess = int(input("Try Again! Pick a number between 1 and 10: "))
+      if guess == answer:
+        print("\nYou did it! It took you {} tries to guess the number".format(tries))
+        print("-------------- ")
+        print("  Game Over!")
+        print("-------------- ")
+        break
+    play_again = input("Would you like to playe again? (Y/N) ")
+    if play_again.lower() == "y":
+      start_game()
+    else:
+      print("Thanks for playing!")
 start_game()
